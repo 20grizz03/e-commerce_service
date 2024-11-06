3b9b6d146e1c4bc586b6e923c38c2c104e4234fb
grizz <sereus2003@gmail.com>
2024-11-06 16:49:59 +0300

    first config

--- app/pkg/models.go
+++ app/pkg/models.go
@@ -1,1 +1,13 @@
 package pkg
+
+import "time"
+
+//Cюда мы прокидывам всек таблицы
+
+type User struct {
+	Id           int
+	Username     string
+	Email        string
+	PasswordHash string
+	CreatedAt    time.Time
+}
