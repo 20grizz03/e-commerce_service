21d1f8141dba0add3965f4e27286140f458246a6
Максим Тверитинов <makstveritinow0809@gmail.com>
2024-11-19 22:27:59 +0300

    added mock

--- app/internal/handlers/handlers.go
+++ app/internal/handlers/handlers.go
@@ -2,6 +2,7 @@ package handlers
 
 import "net/http"
 
+//go:generate mockery --name=Methods --output=../mocks
 type Methods interface {
 	getAllProducts(w http.ResponseWriter, r *http.Request)     // получение всех продуктов
 	getProductsById(w http.ResponseWriter, r *http.Request)    // получение по имени
